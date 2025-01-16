e85829ad55aff315d4578ec79c24d6050b948e89
raphbraga <raphbraga.fcamara@rd.com.br>
2025-01-16 14:18:58 -0300

    swagger integration - openAPI

--- src/main/java/br/com/mounts/order_api/infraestructure/config/OpenAPIConfig.java
+++ src/main/java/br/com/mounts/order_api/infraestructure/config/OpenAPIConfig.java
@@ -4,6 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for OpenAPIConfig
+ *
+ * @author Raphael Braga
+ */
+
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Project Management Orders API", version = "1.0", description = "API para gerenciamento de ordens"))
 public class OpenAPIConfig {
